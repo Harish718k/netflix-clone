9af1fd117209c509681e7343d7e5acbd89f2e21c
Harish718k <harishbca718@gmail.com>
2025-05-21 19:55:29 +0530

    Section added

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -11,15 +11,15 @@ export const AuthScreen = () => {
     <div className="hero-bg relative">
             {/* Navbar */}
         <header className='max-w-6xl mx-auto flex items-center justify-between p-4 pb-10'>
-            <img src="/netflix-logo.png" alt="logo" className='w-32 md:w-52'/>
+            <img src="/netflix-logo.png" alt="logo" className='w-32 md:w-48'/>
             <Link to={"/"} className="text-white bg-red-600 py-1 px-2 rounded">
                 Sign In
             </Link>
         </header>
             {/* hero section */}
         <div className="flex flex-col items-center justify-center text-center py-40 text-white max-w-6xl mx-auto">
-            <h1 className='text-4xl md:text-6xl font-bold mb-4'>Unlimited movies, TV shows, and more</h1>
-            <p className="text-lg mb-4">Watch anywhere. Cancel anytime.</p>
+            <h1 className='text-4xl md:text-6xl font-bold mb-6 max-w-180'>Unlimited movies, TV shows, and more</h1>
+            <p className="text-lg mb-4">Starts at Rs.149. Cancel anytime.</p>
             <p className="mb-4">Ready to watch? Enter your email to create or restart your membership.</p>
 
             <form className='flex flex-col md:flex-row gap-4 w-1/2' onSubmit={handleForm}>
@@ -35,6 +35,36 @@ export const AuthScreen = () => {
                     </button>
             </form>
         </div>
+
+        <div className='h-2 w-full bg-[#232323]'></div>
+
+        {/* 1st section */}
+        <div className='py-10 bg-black text-white'>
+            <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2">
+                <div className="flex-1 text-center md:text-left">
+                    <h2 className='text-4xl md:text-5xl font-extrabold mb-4'>Enjoy on your TV</h2>
+                    <p className="text-lg md:text-xl">
+                        Watch on Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.
+                    </p>
+                </div>
+                <div className="flex-1 relative">
+                    <img src="/tv.png" alt="TV image" className='mt-4 z-20 relative'/>
+                    <video 
+                        className='absolute top-1/2 left-1/2     -translate-x-1/2 -translate-y-1/2 h-1/2 z-10'
+                        type='video/mp4'
+                        playInLine
+                        autoPlay={true}
+                        muted
+                        loop
+                    >
+                        <source src='/hero-vid.m4v' 
+                        
+                        />
+                    </video>
+                </div>
+            </div>
+        </div>
+
     </div>
   )
 }
