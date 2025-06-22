a89eba53ae0384b9d84fd6910fd6e8333ae31688
HARISH <163009721+Harish718k@users.noreply.github.com>
2025-06-22 19:29:46 +0530

    Delete frontend/config/axiosConfig.js

--- frontend/config/axiosConfig.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000", // fallback for local dev
-  withCredentials: true, // if using cookies
-});
-
-export default instance;
